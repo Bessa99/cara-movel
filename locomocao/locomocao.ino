6ab16c37c4276b9f385988e6860fdfdebd466aea
João Daniel de Mélo Miranda <jdmm@cin.ufpe.br>
2026-06-16 17:13:26 -0300

    code atual

--- locomocao/locomocao.ino
+++ locomocao/locomocao.ino
@@ -4,17 +4,34 @@
 #include <Dabble.h>
 #include <Servo.h>
 
-//definindo o servo
+//definindo o servo do tiro
 Servo servo;
 
 //pino do servo 
-int pServo = 11;
+const int pServo = 11;
+
+//definindo servo do ajuste
+Servo servoAjuste;
+
+//pino do servo de ajuste
+const int pinoServoAjuste = 8;
+
+//angulo do servo de ajuste
+int angulo = 90;
+
 
 //definindo os pinos
 const int P1 = 4;
 const int P2 = 5;
 const int P3 = 6;
 const int P4 = 7;
+
+//definindo o pino do motor de corrente contínua e variável ON/OFF
+const int pino_motores = 12;
+bool ligado = false;
+//variavel para o botão quadrado
+bool ultimoEstadoQuadrado = false;
+
 
 //definindo os pinos PWM
 const int A00 = 9;
@@ -23,8 +40,6 @@ const int A01 = 10;
 //definindo a potência das setinhas
 const int potencia_setinhas = 255;
 
-//definindo a potência das setinhas (Curva)
-const int potencia_setinhas_curva = 255;
 
 void setup(){
   //iniciando
@@ -37,25 +52,46 @@ void setup(){
   pinMode(P4, OUTPUT);
   pinMode(A00, OUTPUT);
   pinMode(A01, OUTPUT);
+  pinMode(pino_motores, OUTPUT);
+  pinMode(pinoServoAjuste, OUTPUT);
+
+  //anexando pinos dos servos
+  servo.attach(pServo);
+  servoAjuste.attach(pinoServoAjuste);
+
+  //organizando os angulos inicias dos servos
+  servo.write(75);
+  servoAjuste.write(angulo);
 }
 
 
-void ativar_servo(int pino){
-  if(servo.attached()){
-    servo.detach();
-  }
-  servo.attach(pino);
-  for(int n = 0; n < 90; n++){
-    servo.write(n);
-    delay(2);
-  }
-  for(int n = 90; n > 0; n--){
-    servo.write(n);
-    delay(2);
-  }
-  servo.detach();
+void ativar_servo(bool ligado){
+    for(int n = 75; n > 20; n--){
+      servo.write(n);
+      delay(5);
+    }
+    for(int n = 20; n < 75; n++){
+      servo.write(n);
+      delay(5);
+    }
+
 }
 
+bool ativar_motores(int pino){
+  if (!ligado){
+    ligado = true;
+    digitalWrite(pino, 1);
+  }
+  return true;
+}
+
+bool desativar_motores(int pino){
+  if(ligado){
+    ligado = false;
+    digitalWrite(pino, 0);
+  }
+  return false;
+}
 
 
 void loop(){
@@ -64,8 +100,8 @@ void loop(){
  
   //Frente
   if (GamePad.isUpPressed()){
-    digitalWrite(P1, HIGH);
-    digitalWrite(P2, LOW);
+    digitalWrite(P1, LOW);
+    digitalWrite(P2, HIGH);
     digitalWrite(P3, HIGH);
     digitalWrite(P4, LOW);
 
@@ -75,8 +111,8 @@ void loop(){
 
   //Trás
   else if (GamePad.isDownPressed()){
-    digitalWrite(P1, LOW);
-    digitalWrite(P2, HIGH);
+    digitalWrite(P1, HIGH);
+    digitalWrite(P2, LOW);
     digitalWrite(P3, LOW);
     digitalWrite(P4, HIGH);
 
@@ -88,27 +124,22 @@ void loop(){
   else if (GamePad.isRightPressed()){
     digitalWrite(P1, HIGH);
     digitalWrite(P2, LOW);
-    digitalWrite(P3, LOW);
-    digitalWrite(P4, HIGH);
+    digitalWrite(P3, HIGH);
+    digitalWrite(P4, LOW);
 
-    analogWrite(A00, potencia_setinhas_curva);
-    analogWrite(A01, potencia_setinhas_curva);
-  }
-
-  //ativar servo
-  else if(GamePad.isTrianglePressed()){
-    ativar_servo(pServo);
+    analogWrite(A00, potencia_setinhas);
+    analogWrite(A01, potencia_setinhas);
   }
 
   //Esquerda
   else if (GamePad.isLeftPressed()){
     digitalWrite(P1, LOW);
     digitalWrite(P2, HIGH);
-    digitalWrite(P3, HIGH);
-    digitalWrite(P4, LOW);
+    digitalWrite(P3, LOW);
+    digitalWrite(P4, HIGH);
 
-    analogWrite(A00, potencia_setinhas_curva);
-    analogWrite(A01, potencia_setinhas_curva);
+    analogWrite(A00, potencia_setinhas);
+    analogWrite(A01, potencia_setinhas);
   }
 
   //Parar
@@ -122,6 +153,44 @@ void loop(){
     analogWrite(A01, 0);
   }
 
+  //------ AÇÕES ------//
+
+  bool atualEstadoQuadrado = GamePad.isSquarePressed();
+
+  //ativar servo
+  if(GamePad.isTrianglePressed()){
+    ativar_servo(ligado);
+  }
+
+  //ativar ou desativar motores
+  else if(atualEstadoQuadrado && !ultimoEstadoQuadrado){
+    if(!ligado){
+      ligado = ativar_motores(pino_motores);
+    } else if(ligado){
+      ligado = desativar_motores(pino_motores);
+    }
+  }
+  ultimoEstadoQuadrado = atualEstadoQuadrado;
+
+  //mover torreta para cima
+  if(GamePad.isCrossPressed()){
+    angulo++;
+  }
+
+  //mover torreta para baixo
+  else if(GamePad.isCirclePressed()){
+    angulo--;
+  }
+
+  //limita o angulo entre 90 e 110
+  if(angulo > 110){
+    angulo = 110;
+  }
+  else if(angulo < 90){
+    angulo = 90;
+  }
+
+  servoAjuste.write(angulo);
 
   delay(20);
 }
